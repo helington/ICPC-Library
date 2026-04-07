c87999b6e50adf83da63da8037fbdeec59905b65
helington <willamy.helington@gmail.com>
2026-04-07 09:36:38 -0300

    feat: add spf sieve implementation

--- code/math/sieve.cpp
+++ code/math/sieve.cpp
@@ -1,29 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
 // Sieve of Eratosthenes
 
 // Generates all prime numbers up to MAX_N.
 
 // O(n log(log(n)))
 
-const int MAX_N = 1000005; // Adjust based on problem constraints (usually 1e6 or 1e7)
+const int MAXN = 1000005; // Adjust based on problem constraints (usually 1e6 or 1e7)
 
-bool is_prime[MAX_N];
-vector<int> primes;
-
-void sieve(int n) {
-    fill(is_prime, is_prime + n + 1, true);
+vector<int> sieve() {
+    vector<int> primes;
+    vector<bool> is_prime(MAXN, false);
     is_prime[0] = is_prime[1] = false;
 
-    for (int i = 2; i * i <= n; i++) {
+    for (int i = 2; i <= MAXN; i++) {
         if (is_prime[i]) {
-            // Start from i*i because smaller multiples have already been marked
-            for (int j = i * i; j <= n; j += i)
+            primes.push_back(i);
+            for (int j = 2 * i; j <= MAXN; j += i)
                 is_prime[j] = false;
         }
     }
 
-    for (int i = 2; i <= n; i++) {
-        if (is_prime[i]) {
-            primes.push_back(i);
+    return primes;
+}
+
+// Sieve to get the SPF of the numbers
+
+vector<int> sieve() {
+    vector<int> spf(MAXN, 0);
+    spf[0] = spf[1] = 0;
+
+    for (int i = 2; i <= MAXN; i++) {
+        if (!spf[i]) {
+            spf[i] = i;
+            for (int j = 2 * i; j <= MAXN; j += i)
+                if (!spf[j])
+                    spf[j] = i;
         }
     }
+
+    return spf;
 }
