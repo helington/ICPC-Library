ad40a59239eaf93ceaf8908edbcb85513b325702
hwel <hwel@g2c14.cin.ufpe.br>
2026-05-13 17:26:28 -0300

    feat: improve tarjan implementation

--- code/graph/tarjan.cpp
+++ code/graph/tarjan.cpp
@@ -5,51 +5,51 @@ using namespace std;
 
 // Tarjan's Algorithm (Bridges & Articulation Points)
 
-// Finds all bridges and cut vertices in an UNDIRECTED graph.
-// NOTE: Assumes a simple graph (no parallel edges).
+// Finds all bridges and articulation points (cut vertices) in an UNDIRECTED graph.
+// NOTE: Assumes a simple graph (no parallel edges). To support parallel edges, 
+// pass the edge ID instead of the parent node to the DFS.
 
+// 1-indexed internally.
 // Time Complexity: O(V + E)
 // Space Complexity: O(V + E)
 
 const int MAXN = 1e5 + 5;
-int tin[MAXN], low[MAXN], timer=0;
 vector<int> adj[MAXN];
 int n;
 
 vector<pair<int, int>> bridges;
-vector<int> articulation_points;
+vector<int> art;
 
-void tarjan(int u, int p = -1) {
-    low[u] = tin[u] = timer++;
-
-    int flag = false, children = 0;
-
-    for (auto &v : adj[u]) {
-        if (v == p) continue;
-        
-        if (~tin[v]) {
-            low[u] = min(low[u], tin[v]);
-        } else {
-            tarjan(v, u);
-
-            low[u] = min(low[v], low[u]);
-
-            if (low[v] > tin[u]) bridges.push_back({u, v});
-            if (low[v] >= tin[u]) flag = true;
-            children++;
-        }
-    }
-
-    if (p == -1 && children >= 2) articulation_points.push_back(u);
-    if (~p && flag) articulation_points.push_back(u);
-}
-
-void find_cut_elements() {
-    timer = 0;
+void tarjan() {
+    vector<int> tin(n+1, -1), low(n+1, -1);
+    int timer = 0;
+    
     bridges.clear();
-    articulation_points.clear();
-    memset(tin, -1, sizeof(tin));
+    art.clear();
 
-    for (int i = 0; i < n; i++)
-        if (!~tin[i]) tarjan(i);
+    auto dfs = [&](auto &&dfs, int u, int p = -1) -> void {
+        low[u] = tin[u] = timer++;
+
+        bool is_art = false;
+        int children = 0;
+
+        for (int &v : adj[u]) {
+            if (v == p) continue;
+            
+            if (~tin[v]) low[u] = min(low[u], tin[v]);
+            else {
+                dfs(dfs, v, u);
+                low[u] = min(low[u], low[v]);
+                if (low[v] > tin[u]) bridges.push_back({min(u, v), max(u, v)});
+                if (low[v] >= tin[u]) is_art = true;
+                children++;
+            }
+        }
+
+        if (p == -1 && children >= 2) art.push_back(u);
+        if (~p && is_art) art.push_back(u);
+    };
+
+    for (int u = 1; u <= n; u++)
+        if (tin[u] == -1) dfs(dfs, u);
 }
